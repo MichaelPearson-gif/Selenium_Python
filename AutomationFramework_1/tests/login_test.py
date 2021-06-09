9bc5ce54feaf6c3d26b0e61747cb4965db84de2d
Michael Pearson <mpearson2516@yahoo.com>
2021-06-09 11:15:58 -0700

    Added time.sleep to watch test. Something is wrong with the website password

--- AutomationFramework_1/tests/login_test.py
+++ AutomationFramework_1/tests/login_test.py
@@ -7,29 +7,23 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from AutomationFramework_1.pages.loginPage import LoginPage
 from AutomationFramework_1.pages.homePage import HomePage
 from AutomationFramework_1.utils import utils
+import time
 
+@pytest.mark.usefixtures("test_setup")
 class TestLogin():
 
-    @pytest.fixture(scope="class")
-    def test_setup(self):
-        global driver
-        driver = webdriver.Chrome(ChromeDriverManager().install())
-        driver.implicitly_wait(5)
-        driver.maximize_window()
-        yield
-        driver.close()
-        driver.quit()
-        print("Test completed")
-
-    def test_login(self, test_setup):
+    def test_login(self):
+        driver = self.driver
         driver.get(utils.URL)
 
         login = LoginPage(driver)
         login.enter_username(utils.USERNAME)
         login.enter_password(utils.PASSWORD)
+        time.sleep(2)
         login.click_login()
 
-    def test_logout(self, test_setup):
+    def test_logout(self):
+        driver = self.driver
         homePage = HomePage(driver)
         homePage.click_welcome()
         homePage.click_logout()
